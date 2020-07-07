75f15cc1a4666bdf0e792b530d53fd7769767fa5
dario <gil.q.c.dq@gmail.com>
2020-07-07 09:57:14 -0500

    Revert "carrito"
    This reverts commit adfda2aa3b5a1b2d727053eaeb91b81347bc47ff.

--- php/ventas.php
+++ php/ventas.php
@@ -4,7 +4,7 @@
 
 <?php
 include "conexion.php";
-include 'config.php';
+
 if (empty($_POST['name']))
   $files = get_prod();
 
@@ -30,9 +30,8 @@ if (empty($_POST['name']))
 
 
 <?php require_once "cavecera.php"; ?>
-<div class="col-4" style="margin-bottom: 3%;"></div>
   <section class="container">
-    <div class="col-4 "></div>
+    <div class="col-md-8 "></div>
     <h1>Ventas</h1>
 
   </section>
@@ -40,8 +39,16 @@ if (empty($_POST['name']))
 
   <section class="container">
     
-    
-    
+    <div class="col-4" style="margin-bottom: 3%;"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+        <div class="col-3" style="text-align: right;"> <a href="Classes/generar_archivo.php" class="btn btn-success" > Generar Excel</a>
+        </div>
+      </div>
+    </div>
     <div class="col-4" style="margin-bottom: 3%;"></div>
     <?php if (count($files) > 0) : ?>
 
@@ -51,7 +58,7 @@ if (empty($_POST['name']))
           <table class="table table-striped table-bordered table-hover">
             <thead>
               <tr>
-                <th scope="col-4">
+                <th scope="col">
                 <form method="post" class="form-signin col-12">
                 <h2>Clave</h2>
                     <input type="search" name="name1" class="form-control" placeholder="id" required>
@@ -90,17 +97,9 @@ if (empty($_POST['name']))
                 <td><?php echo $f->clave; ?></td>
                 <td><?php echo $f->nombre; ?></td>
                 <td><?php echo $f->in_act; ?></td>
-             
+                <td>Agregar</td>
                
-
-                <td> <form action="" method="POST">
-                <input type="Text" name="Cantidad" id="Cantidad" value="">
-                    <input type="" name="id" id="id" value="<?php echo  openssl_encrypt($f->clave,code,key); ?>">
-                    <input type="" name="modelo" id="modelo" value="<?php echo openssl_encrypt( $f->nombre,code,key); ?>">
-                    <input type="" name="precio" id="precio" value="<?php echo openssl_encrypt( $f->in_act,code,key); ?>">
-                    <td>  <button class="add-to-cart" name="btnAccion" value="agregar" type="submit" > <em>Comprar</em></button></td>
-                  </form>
-                  </td>
+               
               </tr>
               <?php endforeach; ?>
           </table>
